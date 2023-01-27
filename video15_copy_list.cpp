9a89bccaee4a3dc47ed7dfe9a3358957ca20d19c
Varsha Gupta <89845598+varsha080@users.noreply.github.com>
2023-01-27 23:29:29 +0530

    Create video15_copy_list.cpp

---
+++ video15_copy_list.cpp
@@ -0,0 +1,28 @@
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if (!head) return nullptr;
+
+        // create new nodes and insert them between the original nodes
+        for (auto p = head; p; p = p->next->next) {
+            auto new_node = new Node(p->val);
+            new_node->next = p->next;
+            p->next = new_node;
+        }
+
+        for (auto p = head; p; p = p->next->next) {
+            if (p->random) p->next->random = p->random->next;
+        }
+
+        
+        auto new_head = head->next;
+        for (auto p = head; p; p = p->next) {
+            auto new_node = p->next;
+            p->next = new_node->next;
+            if (new_node->next) new_node->next = new_node->next->next;
+        }
+
+        return new_head;
+    }
+};
