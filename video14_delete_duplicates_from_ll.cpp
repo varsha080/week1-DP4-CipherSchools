6f815215d8bea835f8cd0a379d4a1111ad008ca8
Varsha Gupta <89845598+varsha080@users.noreply.github.com>
2023-01-27 23:21:19 +0530

    Create video14_delete_duplicates_from_ll.cpp

---
+++ video14_delete_duplicates_from_ll.cpp
@@ -0,0 +1,16 @@
+ListNode* deleteAllDuplicates(ListNode* head)
+{
+	ListNode* temp = head;
+	while (temp != nullptr && temp->next != nullptr)
+	{
+		if (temp->val == temp->next->val)
+		{
+			temp->next = temp->next->next;
+		}
+		else
+		{
+			temp = temp->next;
+		}
+	}
+	return head;
+}
