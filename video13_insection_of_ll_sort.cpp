49798583e3ec2945d2f2e13ea8274cb4f82b1cea
Varsha Gupta <89845598+varsha080@users.noreply.github.com>
2023-01-27 23:11:29 +0530

    Create video13_insection_of_ll_sort.cpp

---
+++ video13_insection_of_ll_sort.cpp
@@ -0,0 +1,14 @@
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+		ListNode *temp;
+		while(headA != NULL){
+			temp = headB;
+			while(temp != NULL){
+				if(headA == temp){
+					return headA;
+				}
+				temp = temp -> next;
+			}
+			headA = headA -> next;
+		}
+		return NULL;
+	}
