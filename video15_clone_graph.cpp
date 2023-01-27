b2da59e49ecfd889095232863473a5c044d9e7bc
Varsha Gupta <89845598+varsha080@users.noreply.github.com>
2023-01-27 23:28:20 +0530

    Create video15_clone_graph.cpp

---
+++ video15_clone_graph.cpp
@@ -0,0 +1,13 @@
+
+    unordered_map<Node*, Node*> visited;
+    Node* cloneGraph(Node* node) {
+        if (!node) return nullptr;
+        if (visited.find(node) != visited.end()) return visited[node];
+        
+        auto new_node = new Node(node->val);
+        visited[node] = new_node;
+        for (auto neighbor : node->neighbors) {
+            new_node->neighbors.push_back(cloneGraph(neighbor));
+        }
+        return new_node;
+    }
