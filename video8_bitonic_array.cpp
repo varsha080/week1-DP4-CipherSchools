8ea9e8b7674cbb30c27c6dd442708dfc64fc45bf
Varsha Gupta <89845598+varsha080@users.noreply.github.com>
2023-01-27 22:22:05 +0530

    Create video8_bitonic_array.cpp

---
+++ video8_bitonic_array.cpp
@@ -0,0 +1,14 @@
+int findMax(vector<int>& nums) {
+    int left = 0, right = nums.size() - 1;
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) {
+            return nums[mid];
+        } else if (nums[mid] > nums[mid-1] && nums[mid] < nums[mid+1]) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1; 
+}
